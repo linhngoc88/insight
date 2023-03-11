098731504eaeba3599537d9ce3924312e2c460fd
Linh Ngoc <lynhngoc88@gmail.com>
2023-03-11 16:40:58 +0700

    Add docs

--- include/insight/insight_matrix_int.h
+++ include/insight/insight_matrix_int.h
@@ -3,6 +3,20 @@
 
 #include <stdlib.h>
 
+// Row-major (see [1]) matrix structure.
+//
+//                              +-------------...------------+
+//   +------------------+       |           storage          |
+//   |     elem         |-----> +-------------...------------+
+//   +------------------+
+//   |     owners       |
+//   +------------------+
+//   |     num_cols     |
+//   +------------------+
+//   |     num_rows     |
+//   +------------------+
+//
+// [1] - https://en.wikipedia.org/wiki/Row-_and_column-major_order
 typedef struct {
   size_t num_rows;
   size_t num_cols;
@@ -12,22 +26,29 @@ typedef struct {
 
 /* Allocation */
 
-// TODO(linh): add docs
+// Allocates memory for a num_rows x num_cols matrix. All elements of the
+// matrix are uninitialized and the matrix owns these elements.
 insight_matrix_int *
 insight_matrix_int_alloc(const size_t num_rows, const size_t num_cols);
 
-// TODO(linh): add docs
+// Allocates memory for a num_rows x num_cols matrix. All elements of the
+// matrix are initialized to zero and the matrix owns these elements.
 insight_matrix_int *
 insight_matrix_int_calloc(const size_t num_rows, const size_t num_cols);
 
-// TODO(linh): add docs
+// Deallocates memory occupied by the given matrix.
 void insight_matrix_int_free(insight_matrix_int * matrix);
 
 /* Operations */
 
-// TODO(linh): add docs
+// Sets all elements of the given matrix to zero.
 void insight_matrix_int_set_zero(insight_matrix_int * matrix);
+
+// Set emements of the given matrix to one if they lie on the main diagonal or
+// to zero if otherwise.
 void insight_matrix_int_set_identity(insight_matrix_int * matrix);
+
+// Set all elements of the given matrix to the specified value.
 void insight_matrix_int_set_value(insight_matrix_int * matrix, int value);
 
 #endif /* INSIGHT_MATRIX_INT_H_ */

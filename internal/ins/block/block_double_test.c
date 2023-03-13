b1a60574f43548e639e9f8f7611329fe595c3c5e
Linh Ngoc <lynhngoc88@gmail.com>
2023-03-14 00:47:12 +0700

    Init tests for block_double

---
+++ internal/ins/block/block_double_test.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <ins/ins_block.h>
+
+
+static void alloc_success(void **state) {
+  (void) state; /* unused */
+
+  ins_block * block = ins_block_alloc(3);
+
+  assert_non_null(block);
+  assert_non_null(block->base);
+  assert_int_equal(block->size, 3);
+}
+
+int main(void) {
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test(alloc_success)
+  };
+
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}

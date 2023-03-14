19479d5e21947a31776f14d52ebf83fe5305babb
Linh Ngoc <lynhngoc88@gmail.com>
2023-03-14 21:24:07 +0700

    Test block calloc

--- internal/ins/block/block_double_test.c
+++ internal/ins/block/block_double_test.c
@@ -10,17 +10,29 @@ static void alloc_success(void **state) {
   (void) state; /* unused */
 
   ins_block * block = ins_block_alloc(3);
+  assert_non_null(block);
+  assert_non_null(block->data);
+  assert_int_equal(block->size, 3);
+  ins_block_free(block);
+}
 
+static void calloc_success(void **state) {
+  (void) state; /* unused */
+
+  ins_block * block = ins_block_calloc(3);
   assert_non_null(block);
   assert_non_null(block->data);
   assert_int_equal(block->size, 3);
 
+  double expected_mem[] = {0.0, 0.0, 0.0};
+  assert_memory_equal(block->data, expected_mem, block->size * sizeof(double));
   ins_block_free(block);
 }
 
 int main(void) {
   const struct CMUnitTest tests[] = {
-    cmocka_unit_test(alloc_success)
+    cmocka_unit_test(alloc_success),
+    cmocka_unit_test(calloc_success)
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);

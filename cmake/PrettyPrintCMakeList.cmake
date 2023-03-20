64be39b5e35bc44985f83e2db9161138be2192a3
Linh Ngoc <lynhngoc88@gmail.com>
2023-03-20 10:26:27 +0700

    discontinue this branch

---
+++ cmake/PrettyPrintCMakeList.cmake
@@ -0,0 +1,9 @@
+# pretty_print_cmake_list(OUTPUT_VAR [item1 [item2 ...]])
+#
+# Sets ${OUTPUT_VAR} in the caller's scope to a human-readable string
+# represenation of the list passed as the remaining argument formed
+# as: "[item1, item2, ..., itemN]".
+function(pretty_print_cmake_list OUTPUT_VAR)
+  string(REPLACE ";" ", " PRETTY_LIST_STRINGS "[${ARGN}]")
+  set(${OUTPUT_VAR} ${PRETTY_LIST_STRINGS} PARENT_SCOPE)
+endfunction()

50d76583b2e09efbfb04de03c733bec5938986c9
Linh Ngoc <lynhngoc88@gmail.com>
2023-03-25 22:49:37 +0700

    add two vectors operation

--- internal/ins/vector/vector_double_oper_test.c
+++ internal/ins/vector/vector_double_oper_test.c
@@ -116,11 +116,165 @@ static void test_scale_when_alloc_from_block_stride_two(void **state) {
   ins_block_free(b);
 }
 
+static void test_vector_add_two_vectors_same_stride_one(void **state) {
+  (void) state;
+
+  ins_vector *x = ins_vector_alloc(3);
+  ins_vector *y = ins_vector_alloc(3);
+
+  ins_vector_set(x, 0, 0.5);
+  ins_vector_set(x, 1, 2.0);
+  ins_vector_set(x, 2, -1.5);
+
+  ins_vector_set(y, 0, 1.0);
+  ins_vector_set(y, 1, 0.4);
+  ins_vector_set(y, 2, 0.5);
+
+  const int ret  = ins_vector_add(x, y);
+  assert_int_equal(ret, INS_SUCCESS);
+
+  // Check x data
+  assert_double_equal(ins_vector_get(x, 0), 1.5, 0.0);
+  assert_double_equal(ins_vector_get(x, 1), 2.4, 0.0);
+  assert_double_equal(ins_vector_get(x, 2), -1.0, 0.0);
+
+  // Check y data
+  assert_double_equal(ins_vector_get(y, 0), 1.0, 0.0);
+  assert_double_equal(ins_vector_get(y, 1), 0.4, 0.0);
+  assert_double_equal(ins_vector_get(y, 2), 0.5, 0.0);
+
+  ins_vector_free(y);
+  ins_vector_free(x);
+}
+
+static void test_vector_add_two_vectors_same_stride_two(void **state) {
+  (void) state;
+
+  ins_vector *x = ins_vector_alloc(5);
+
+  x->data[0] = 0.05;
+  x->data[1] = -1.0;
+  x->data[2] = 1.0;
+  x->data[3] = 4.5;
+  x->data[4] = 6.0;
+
+  x->size = 3;
+  x->stride = 2;
+
+  ins_vector *y = ins_vector_alloc(5);
+
+  y->data[0] = 1.2;
+  y->data[1] = 0.0;
+  y->data[2] = 2.0;
+  y->data[3] = 0.5;
+  y->data[4] = 0.75;
+
+
+  y->size = 3;
+  y->stride = 2;
+
+  const int ret = ins_vector_add(x, y);
+  assert_int_equal(ret, INS_SUCCESS);
+
+  // Check x elements
+  assert_double_equal(ins_vector_get(x, 0), 1.25, 0.0);
+  assert_double_equal(ins_vector_get(x, 1), 3.0, 0.0);
+  assert_double_equal(ins_vector_get(x, 2), 6.75, 0.0);
+
+  // Check y elements
+  assert_double_equal(ins_vector_get(y, 0), 1.2, 0.0);
+  assert_double_equal(ins_vector_get(y, 1), 2.0, 0.0);
+  assert_double_equal(ins_vector_get(y, 2), 0.75, 0.0);
+
+  // Check x data
+  assert_double_equal(x->data[0], 1.25, 0.0);
+  assert_double_equal(x->data[1], -1.0, 0.0);
+  assert_double_equal(x->data[2], 3.0, 0.0);
+  assert_double_equal(x->data[3], 4.5, 0.0);
+  assert_double_equal(x->data[4], 6.75, 0.0);
+
+  // Check y data
+  assert_double_equal(y->data[0], 1.2, 0.0);
+  assert_double_equal(y->data[1], 0.0, 0.0);
+  assert_double_equal(y->data[2], 2.0, 0.0);
+  assert_double_equal(y->data[3], 0.5, 0.0);
+  assert_double_equal(y->data[4], 0.75, 0.0);
+
+  ins_vector_free(y);
+  ins_vector_free(x);
+}
+
+static void test_vector_add_two_vectors_different_strides(void **state) {
+  (void) state;
+
+  ins_vector *x = ins_vector_alloc(3);
+  x->data[0] = 0.5;
+  x->data[1] = 2.0;
+  x->data[2] = -1.5;
+
+  ins_vector *y = ins_vector_alloc(5);
+  y->data[0] = 1.2;
+  y->data[1] = 0.0;
+  y->data[2] = 2.0;
+  y->data[3] = 0.5;
+  y->data[4] = 0.75;
+
+  y->size = 3;
+  y->stride = 2;
+
+  const int ret = ins_vector_add(x, y);
+  assert_int_equal(ret, INS_SUCCESS);
+
+  // Check x elements
+  assert_double_equal(ins_vector_get(x, 0), 1.7, 0.0);
+  assert_double_equal(ins_vector_get(x, 1), 4.0, 0.0);
+  assert_double_equal(ins_vector_get(x, 2), -0.75, 0.0);
+
+  // Check y elements
+  assert_double_equal(ins_vector_get(y, 0), 1.2, 0.0);
+  assert_double_equal(ins_vector_get(y, 1), 2.0, 0.0);
+  assert_double_equal(ins_vector_get(y, 2), 0.75, 0.0);
+
+  // Check x data
+  assert_double_equal(x->data[0], 1.7, 0.0);
+  assert_double_equal(x->data[1], 4.0, 0.0);
+  assert_double_equal(x->data[2], -0.75, 0.0);
+
+  // Check y data
+  assert_double_equal(y->data[0], 1.2, 0.0);
+  assert_double_equal(y->data[1], 0.0, 0.0);
+  assert_double_equal(y->data[2], 2.0, 0.0);
+  assert_double_equal(y->data[3], 0.5, 0.0);
+  assert_double_equal(y->data[4], 0.75, 0.0);
+
+  ins_vector_free(y);
+  ins_vector_free(x);
+}
+
+static void test_vector_add_two_vectors_different_lengths(void **state) {
+  (void) state;
+
+  ins_vector *x = ins_vector_alloc(3);
+  ins_vector *y = ins_vector_alloc(2);
+
+  ins_set_error_handler_off();
+
+  const int ret = ins_vector_add(x, y);
+  assert_int_equal(ret, INS_EBADLEN);
+
+  ins_vector_free(y);
+  ins_vector_free(x);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] = {
     cmocka_unit_test(test_scale_when_stride_is_one),
     cmocka_unit_test(test_scale_when_alloc_from_block_stride_one),
-    cmocka_unit_test(test_scale_when_alloc_from_block_stride_two)
+    cmocka_unit_test(test_scale_when_alloc_from_block_stride_two),
+    cmocka_unit_test(test_vector_add_two_vectors_same_stride_one),
+    cmocka_unit_test(test_vector_add_two_vectors_same_stride_two),
+    cmocka_unit_test(test_vector_add_two_vectors_different_strides),
+    cmocka_unit_test(test_vector_add_two_vectors_different_lengths)
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);

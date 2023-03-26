958946f78acb65a5d13fc66e458bbfffa66599ba
Linh Ngoc <lynhngoc88@gmail.com>
2023-03-26 20:51:34 +0700

    subtract one vector from another operation

--- internal/ins/vector/oper_source.c
+++ internal/ins/vector/oper_source.c
@@ -17,6 +17,25 @@ int INS_VECTOR_FUNC(add)(INS_VECTOR_TYPE * x, const INS_VECTOR_TYPE * y) {
   return INS_SUCCESS;
 }
 
+int INS_VECTOR_FUNC(sub)(INS_VECTOR_TYPE * x, const INS_VECTOR_TYPE * y) {
+  const size_t size = x->size;
+
+  if (y->size != size) {
+    INS_ERROR("vectors must have same length", INS_EBADLEN);
+  }
+
+  const size_t x_stride = x->stride;
+  const size_t y_stride = y->stride;
+
+  size_t i;
+
+  for (i = 0; i < size; ++i) {
+    x->data[i * x_stride] -= y->data[i * y_stride];
+  }
+
+  return INS_SUCCESS;
+}
+
 int INS_VECTOR_FUNC(scale)(INS_VECTOR_TYPE * x, INS_NUMERIC_TYPE alpha) {
   const size_t n = x->size;
   const size_t stride = x->stride;
--- internal/ins/vector/vector_double_oper_test.c
+++ internal/ins/vector/vector_double_oper_test.c
@@ -266,6 +266,160 @@ static void test_vector_add_two_vectors_different_lengths(void **state) {
   ins_vector_free(x);
 }
 
+static void test_vector_sub_same_stride_one(void **state) {
+  (void) state;
+
+  ins_vector *x = ins_vector_alloc(3);
+  x->data[0] = 1.0;
+  x->data[1] = 4.5;
+  x->data[2] = -0.25;
+
+  ins_vector *y = ins_vector_alloc(3);
+  y->data[0] = 0.5;
+  y->data[1] = 2.0;
+  y->data[2] = 0.25;
+
+  const int ret = ins_vector_sub(x, y);
+  assert_int_equal(ret, INS_SUCCESS);
+
+  // Check x's elements.
+  assert_double_equal(ins_vector_get(x, 0), 0.5, 0.0);
+  assert_double_equal(ins_vector_get(x, 1), 2.5, 0.0);
+  assert_double_equal(ins_vector_get(x, 2), -0.5, 0.0);
+
+  // Check y's elements.
+  assert_double_equal(ins_vector_get(y, 0), 0.5, 0.0);
+  assert_double_equal(ins_vector_get(y, 1), 2.0, 0.0);
+  assert_double_equal(ins_vector_get(y, 2), 0.25, 0.0);
+
+  ins_vector_free(y);
+  ins_vector_free(x);
+}
+
+static void test_vector_sub_same_stride_two(void **state) {
+  (void) state;
+
+  ins_vector *x = ins_vector_alloc(7);
+  x->data[0] = 0.25;
+  x->data[1] = 1.7;
+  x->data[2] = 1.4;
+  x->data[3] = 12.35;
+  x->data[4] = -0.25;
+  x->data[5] = 2.15;
+  x->data[6] = 3.14;
+
+  x->size = 3;
+  x->stride = 2;
+
+  ins_vector *y = ins_vector_alloc(7);
+  y->data[0] = 2.0;
+  y->data[1] = -0.12;
+  y->data[2] = 0.2;
+  y->data[3] = 0.35;
+  y->data[4] = -4.0;
+  y->data[5] = 1.25;
+  y->data[6] = 0.14;
+
+  y->size = 3;
+  y->stride = 2;
+
+  const int ret = ins_vector_sub(x, y);
+  assert_int_equal(ret, INS_SUCCESS);
+
+  // Check x's elements
+  assert_double_equal(ins_vector_get(x, 0), -1.75, 0.0);
+  assert_double_equal(ins_vector_get(x, 1), 1.2, 0.0);
+  assert_double_equal(ins_vector_get(x, 2), 3.75, 0.0);
+
+  // Check y's elements
+  assert_double_equal(ins_vector_get(y, 0), 2.0, 0.0);
+  assert_double_equal(ins_vector_get(y, 1), 0.2, 0.0);
+  assert_double_equal(ins_vector_get(y, 2), -4.0, 0.0);
+
+  // Check x's data
+  assert_double_equal(x->data[0], -1.75, 0.0);
+  assert_double_equal(x->data[1], 1.7, 0.0);
+  assert_double_equal(x->data[2], 1.2, 0.0);
+  assert_double_equal(x->data[3], 12.35, 0.0);
+  assert_double_equal(x->data[4], 3.75, 0.0);
+  assert_double_equal(x->data[5], 2.15, 0.0);
+  assert_double_equal(x->data[6], 3.14, 0.0);
+
+  // Check y's data
+  assert_double_equal(y->data[0], 2.0, 0.0);
+  assert_double_equal(y->data[1], -0.12, 0.0);
+  assert_double_equal(y->data[2], 0.2, 0.0);
+  assert_double_equal(y->data[3], 0.35, 0.0);
+  assert_double_equal(y->data[4], -4.0, 0.0);
+  assert_double_equal(y->data[5], 1.25, 0.0);
+  assert_double_equal(y->data[6], 0.14, 0.0);
+
+  ins_vector_free(y);
+  ins_vector_free(x);
+}
+
+static void test_vector_sub_different_strides(void **state) {
+  (void) state;
+
+  ins_vector *x = ins_vector_alloc(3);
+  x->data[0] = 0.5;
+  x->data[1] = 2.0;
+  x->data[2] = -1.5;
+
+  ins_vector *y = ins_vector_alloc(5);
+  y->data[0] = 1.2;
+  y->data[1] = 0.0;
+  y->data[2] = 2.0;
+  y->data[3] = 0.5;
+  y->data[4] = 0.75;
+
+  y->size = 3;
+  y->stride = 2;
+
+  const int ret = ins_vector_sub(x, y);
+  assert_int_equal(ret, INS_SUCCESS);
+
+  // Check x's elements
+  assert_double_equal(ins_vector_get(x, 0), -0.7, 0.0);
+  assert_double_equal(ins_vector_get(x, 1), 0.0, 0.0);
+  assert_double_equal(ins_vector_get(x, 2), -2.25, 0.0);
+
+  // Check y's elements
+  assert_double_equal(ins_vector_get(y, 0), 1.2, 0.0);
+  assert_double_equal(ins_vector_get(y, 1), 2.0, 0.0);
+  assert_double_equal(ins_vector_get(y, 2), 0.75, 0.0);
+
+  // Check x's data
+  assert_double_equal(x->data[0], -0.7, 0.0);
+  assert_double_equal(x->data[1], 0.0, 0.0);
+  assert_double_equal(x->data[2], -2.25, 0.0);
+
+  // Check y's data
+  assert_double_equal(y->data[0], 1.2, 0.0);
+  assert_double_equal(y->data[1], 0.0, 0.0);
+  assert_double_equal(y->data[2], 2.0, 0.0);
+  assert_double_equal(y->data[3], 0.5, 0.0);
+  assert_double_equal(y->data[4], 0.75, 0.0);
+
+  ins_vector_free(y);
+  ins_vector_free(x);
+}
+
+static void test_vector_sub_different_lengths(void **state) {
+  (void) state;
+
+  ins_vector *x = ins_vector_alloc(3);
+  ins_vector *y = ins_vector_alloc(2);
+
+  ins_set_error_handler_off();
+
+  const int ret = ins_vector_sub(x, y);
+  assert_int_equal(ret, INS_EBADLEN);
+
+  ins_vector_free(y);
+  ins_vector_free(x);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] = {
     cmocka_unit_test(test_scale_when_stride_is_one),
@@ -274,7 +428,11 @@ int main(void) {
     cmocka_unit_test(test_vector_add_two_vectors_same_stride_one),
     cmocka_unit_test(test_vector_add_two_vectors_same_stride_two),
     cmocka_unit_test(test_vector_add_two_vectors_different_strides),
-    cmocka_unit_test(test_vector_add_two_vectors_different_lengths)
+    cmocka_unit_test(test_vector_add_two_vectors_different_lengths),
+    cmocka_unit_test(test_vector_sub_same_stride_one),
+    cmocka_unit_test(test_vector_sub_same_stride_two),
+    cmocka_unit_test(test_vector_sub_different_strides),
+    cmocka_unit_test(test_vector_sub_different_lengths)
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);

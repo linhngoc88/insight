234f8062a746d5a4fbd533a9a407563d3007ee5d
Linh Ngoc <lynhngoc88@gmail.com>
2023-04-01 22:47:32 +0700

    Define ins_vector_max for finding maximum value in a vector

--- internal/ins/vector/minmax_source.c
+++ internal/ins/vector/minmax_source.c
@@ -27,3 +27,33 @@ INS_VECTOR_FUNC(min)(const INS_VECTOR_TYPE *v) {
 
   return min;
 }
+
+INS_NUMERIC_TYPE
+INS_VECTOR_FUNC(max)(const INS_VECTOR_TYPE *v) {
+  const size_t size = v->size;
+  const size_t stride = v->stride;
+
+  INS_NUMERIC_TYPE max = v->data[0 * stride];
+  INS_NUMERIC_TYPE cur;
+
+  size_t i;
+
+  for (i = 0; i < size; ++i) {
+    cur = v->data[i * stride];
+
+    if (cur > max) {
+      max = cur;
+    }
+
+#ifdef INS_FLOATING_POINT
+
+    if (isnan(cur)) {
+      return cur;
+    }
+
+#endif
+
+  }
+
+  return max;
+}
--- internal/ins/vector/vector_double_minmax_test.c
+++ internal/ins/vector/vector_double_minmax_test.c
@@ -71,11 +71,80 @@ static void test_vector_min_nan(void **state) {
   ins_vector_free(v);
 }
 
+static void test_vector_max_stride_one(void **state) {
+  (void) state;
+
+  ins_vector *v = ins_vector_alloc(3);
+
+  v->data[0] = 1.0;
+  v->data[1] = 0.5;
+  v->data[2] = 4.0;
+
+  assert_double_equal(ins_vector_max(v), 4.0, 0.0);
+
+  v->data[0] = 4.25;
+  assert_double_equal(ins_vector_max(v), 4.25, 0.0);
+
+  v->data[2] = 10.0;
+  assert_double_equal(ins_vector_max(v), 10.0, 0.0);
+
+  v->data[1] = 16.0;
+  assert_double_equal(ins_vector_max(v), 16.0, 0.0);
+
+  ins_vector_free(v);
+}
+
+static void test_vector_max_stride_two(void **state) {
+  (void) state;
+
+  ins_vector *v = ins_vector_alloc(5);
+
+  v->data[0] = 1.0;
+  v->data[1] = 2.0;
+  v->data[2] = 0.5;
+  v->data[3] = 7.5;
+  v->data[4] = 4.0;
+
+  v->size = 3;
+  v->stride = 2;
+
+  assert_double_equal(ins_vector_max(v), 4.0, 0.0);
+
+  ins_vector_set(v, 0, 4.25);
+  assert_double_equal(ins_vector_max(v), 4.25, 0.0);
+
+  ins_vector_set(v, 2, 10.0);
+  assert_double_equal(ins_vector_max(v), 10.0, 0.0);
+
+  ins_vector_set(v, 1, 16.0);
+  assert_double_equal(ins_vector_max(v), 16.0, 0.0);
+
+  ins_vector_free(v);
+}
+
+static void test_vector_max_nan(void **state) {
+  (void) state;
+
+  ins_vector *v = ins_vector_alloc(3);
+
+  v->data[0] = 1.0;
+  v->data[1] = sqrt(-4.0);
+  v->data[2] = 4.5;
+
+  const double max = ins_vector_max(v);
+  assert_int_equal(isnan(max), 1);
+
+  ins_vector_free(v);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] = {
     cmocka_unit_test(test_vector_min_stride_one),
     cmocka_unit_test(test_vector_min_stride_two),
-    cmocka_unit_test(test_vector_min_nan)
+    cmocka_unit_test(test_vector_min_nan),
+    cmocka_unit_test(test_vector_max_stride_one),
+    cmocka_unit_test(test_vector_max_stride_two),
+    cmocka_unit_test(test_vector_max_nan)
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
